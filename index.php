fd25149e891f23ca08074d875105f03bbafff7db
unknown <mlanjanasiphamandla@gmail.com>
2023-08-21 13:51:33 +0200

    add to cart

--- index.php
+++ index.php
@@ -1,11 +1,49 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 $connection = mysqli_connect('localhost', 'root', '', 'products');
 
-$sql = mysqli_query($connection, "SELECT * FROM sneakers");
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
-$results = mysqli_fetch_all($sql, MYSQLI_ASSOC);
-
-exit(json_encode($results));
-
+if (isset($_POST['id']) && isset($_POST['quantity'])) {
+    // Add to cart functionality
+    $product_id = $_POST['id'];
+    $quantity = $_POST['quantity'];
+    
+    $product_id = mysqli_real_escape_string($connection, $product_id);
+    $quantity = mysqli_real_escape_string($connection, $quantity);
+    
+    $cart_query = "SELECT * FROM cart WHERE id = $product_id";
+    $cart_result = mysqli_query($connection, $cart_query);
+    
+    if (mysqli_num_rows($cart_result) > 0) {
+        $update_query = "UPDATE cart SET quantity = quantity + $quantity WHERE id = $product_id";
+        mysqli_query($connection, $update_query);
+    } else {
+        $insert_query = "INSERT INTO cart (id, quantity) VALUES ($product_id, $quantity)";
+        mysqli_query($connection, $insert_query);
+    }
+    
+    mysqli_close($connection);
+    
+    $response = array("message" => "Product added to cart successfully");
+    echo json_encode($response);
+} elseif (isset($_GET['id'])) {
+    // Retrieve single sneaker details
+    $sneakerId = $_GET['id'];
+    $SingleSneakerSql = mysqli_prepare($connection, "SELECT * FROM sneakers WHERE id = ?");
+    mysqli_stmt_bind_param($SingleSneakerSql, "i", $sneakerId);
+    mysqli_stmt_execute($SingleSneakerSql);
+    $SingleSneakerResults = mysqli_stmt_get_result($SingleSneakerSql);
+    $sneaker = mysqli_fetch_assoc($SingleSneakerResults);
+    exit(json_encode($sneaker));
+} else {
+    // Retrieve all sneakers
+    $sql = mysqli_query($connection, "SELECT * FROM sneakers");
+    $results = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+    exit(json_encode($results));
+}
 ?>
--- main.js
+++ main.js
@@ -2,41 +2,28 @@ fetch('./index.php')
 .then((res) => res.json())
 .then(response => {
     let output = '';
-    // let sneakerOutput = '';
     for(let i in response) {
         output += `
-                    <tr>
-                        <td>${response[i].id}</td>
-                        <td>${response[i].brand}</td>
-                        <td>${response[i].model}</td>
-                        <td>$${response[i].price}</td>
-                        <td class="del">Delete</td>
-                        <td class="edit">Edit</td>
-                    </tr>
+            <tr>
+                <td>${response[i].id}</td>
+                <td>${response[i].brand}</td>
+                <td>${response[i].model}</td>
+                <td>$${response[i].price}</td>
+                <td class="del">Delete</td>
+                <td class="edit">Edit</td>
+            </tr>
         `;
-
-        // sneakerOutput += `
-        //             <div class="sneaker" data-id="${response[sneaker].id}">
-        //                     <img src="${response[i].imageUrl}" alt="${response[i].brand} ${response[i].model}" width="200">
-        //                     <h3>${response[i].brand} ${response[i].model}</h3>
-        //                     <p>Price: $${response[i].price}</p>
-        //             </div>
-        // `;
     }
     
-     // For index.html
     if (document.querySelector('tbody')) {
         document.querySelector('tbody').innerHTML = output;
     }
     
-    // For products.html
     if (document.querySelector('.sneakers')) {
         DisplayAllSneakers(response);
-        DisplaySingleSneaker(response);
     }
 
 }).catch(error => console.error(error));
-
 
 function DisplayAllSneakers(response) { 
     let output = '';
@@ -44,12 +31,10 @@ function DisplayAllSneakers(response) {
     for (let sneaker in response) {
         output += `
             <div class="sneaker" data-id="${response[sneaker].id}">
-                <a href="./single_product.html" target="_blank">
-                    <img src="${response[sneaker].imageUrl}" alt="${response[sneaker].brand} ${response[sneaker].model}" width="200">
-                    <h3>${response[sneaker].model}</h3> 
-                    <h4>${response[sneaker].brand}</h4>
-                    <p>Price: $${response[sneaker].price}</p>
-                </a>
+                <img src="${response[sneaker].imageUrl}" alt="${response[sneaker].brand} ${response[sneaker].model}" width="200">
+                <h3>${response[sneaker].model}</h3> 
+                <h4>${response[sneaker].brand}</h4>
+                <p>Price: $${response[sneaker].price}</p>
             </div>
         `;
     }
@@ -58,7 +43,6 @@ function DisplayAllSneakers(response) {
 
 function DisplaySingleSneaker(response) {
     let sneakers = document.querySelectorAll('.sneaker');
-    window.location.href = '';
     
     sneakers.forEach(sneaker => {
         sneaker.addEventListener("click", () => {
@@ -74,25 +58,6 @@ function DisplaySingleSneaker(response) {
     });
 }
 
-
 function displaySneakerDetails(sneaker) {
-    const sneakerDetails = document.querySelector('.product-details');
-    let output = "";
-    
     window.location.href = `single_product.html?id=${sneaker.id}`;
-    
-    output += `
-            <img src="${sneaker.imageUrl}" alt="${sneaker.brand} ${sneaker.model}" width="300">
-                <h2>${sneaker.model}</h2>
-                <h3>${sneaker.brand}</h3>
-                <p>Price: $${sneaker.price}</p>
-                <p>Colorway: ${sneaker.colorway}</p>
-                <p>Release Date: ${sneaker.releaseDate}</p>
-                <p>Size Range: ${sneaker.sizeRange}</p>
-                <p>Stock Quantity: ${sneaker.stockQuantity}</p>
-    `;
-
-    sneakerDetails.innerHTML = output;
 }
-
-
